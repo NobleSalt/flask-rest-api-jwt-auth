604bfd32528d1f4419a4cce1211798b093843ae4
Emmanuel Izuchukwu <izuchukwue2@gmail.com>
2022-06-19 20:55:43 +0100

    heroku push

--- app.py
+++ app.py
@@ -53,3 +53,7 @@ def token_required(f):
 
             return f(current_user, *args,  **kwargs)
     return decorator
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
